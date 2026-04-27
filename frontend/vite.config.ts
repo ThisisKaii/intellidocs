c9522e4d825c07defcd4b51c72f127c9625fb377
Joshua <joshuaasingua499@gmail.com>
2026-04-28 04:16:27 +0800

    added allowHosted for a cloudflare tunnel domain

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,9 @@ export default defineConfig({
     },
   },
   server: {
+    host: true,
     port: 5173,
+    allowedHosts: ['.trycloudflare.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
